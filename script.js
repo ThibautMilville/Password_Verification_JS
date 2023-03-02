0d5d1d21ba7bb724e50f6190d0f26bb2dd43ae71
ThibautMilville <tmilville.pro@gmail.com>
2023-03-02 20:50:26 +0100

    variables

---
+++ script.js
@@ -0,0 +1,6 @@
+// Définir les variables
+var myInput = document.getElementById('psw')
+var letter = document.getElementById('letter')
+var capital = document.getElementById('capital')
+var number = document.getElementById('number')
+var length = document.getElementById('length')
